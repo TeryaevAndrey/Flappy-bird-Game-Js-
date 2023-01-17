66718517885d46ed7acd4d6b513ed9a35970de83
Andrey <teryaev.2004@mail.ru>
2023-01-17 22:59:10 +0300

    add-restart-and-fix-jump-and-fix-scalable-for-mob

--- app.js
+++ app.js
@@ -10,16 +10,34 @@ const pipeBottom = new Image();
 let gap = 90;
 
 const moveUp = () => {
-  yPos -= 40;
-}
+  yPos -= 45;
+};
+
+const restartGame = (e) => {
+  e.preventDefault();
+
+  e.target.blur();
+
+  xPos = 10;
+  yPos = 150;
+
+  pipe = [];
+  pipe[0] = {
+    x: cvs.width,
+    y: 0,
+  };
+
+  score = 0;
+};
 
 document.addEventListener("keydown", moveUp);
 document.addEventListener("touchstart", moveUp);
+document.getElementById("restart").addEventListener("click", restartGame);
 
-const pipe = [];
+let pipe = [];
 pipe[0] = {
   x: cvs.width,
-  y: 0
+  y: 0,
 };
 
 let score = 0;
@@ -28,42 +46,58 @@ let xPos = 10;
 let yPos = 150;
 let grav = 2;
 
-bird.src="img/user.png";
-bg.src="img/bg.png";
-fg.src="img/road.png";
-pipeUp.src="img/top.png";
-pipeBottom.src="img/bottom.png";
+bird.src = "img/user.png";
+bg.src = "img/bg.png";
+fg.src = "img/road.png";
+pipeUp.src = "img/top.png";
+pipeBottom.src = "img/bottom.png";
 
 const score_audio = new Audio();
 
-score_audio.src="audio/score.mp3";
+score_audio.src = "audio/score.mp3";
 
 const draw = () => {
   ctx.drawImage(bg, 0, 0);
 
-  for(let i = 0; i < pipe.length; i++) {
+  for (let i = 0; i < pipe.length; i++) {
     ctx.drawImage(pipeUp, pipe[i].x, pipe[i].y);
     ctx.drawImage(pipeBottom, pipe[i].x, pipe[i].y + pipeUp.height + gap);
 
     pipe[i].x--;
 
-    if(pipe[i].x === 125) {
+    if (pipe[i].x === 125) {
       pipe.push({
         x: cvs.width,
-        y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height
+        y: Math.floor(Math.random() * pipeUp.height) - pipeUp.height,
       });
     }
 
-    if(xPos + bird.width >= pipe[i].x && xPos <= pipe[i].x + pipeUp.width && (yPos <= pipe[i].y + pipeUp.height || yPos + bird.height >= pipe[i].y + pipeUp.height + gap) || yPos + bird.height >= cvs.height - fg.height || yPos <= 0) {
-      location.reload();
+    if (
+      (xPos + bird.width >= pipe[i].x &&
+        xPos <= pipe[i].x + pipeUp.width &&
+        (yPos <= pipe[i].y + pipeUp.height ||
+          yPos + bird.height >= pipe[i].y + pipeUp.height + gap)) ||
+      yPos + bird.height >= cvs.height - fg.height ||
+      yPos <= 0
+    ) {
+      xPos = 10;
+      yPos = 150;
+
+      pipe = [];
+      pipe[0] = {
+        x: cvs.width,
+        y: 0,
+      };
+
+      score = 0;
     }
 
-    if(pipe[i].x === 5) {
+    if (pipe[i].x === 5) {
       score++;
       score_audio.play();
     }
   }
-  
+
   ctx.drawImage(fg, 0, cvs.height - fg.height);
   ctx.drawImage(bird, xPos, yPos);
 
@@ -71,9 +105,9 @@ const draw = () => {
 
   ctx.fillStyle = "#000";
   ctx.font = "24px Verdana";
-  ctx.fillText("Счет: " + score, 10, cvs.height - 20)
+  ctx.fillText("Счет: " + score, cvs.width / 2 - 40, cvs.height - 10);
+};
 
-  requestAnimationFrame(draw);
-}
+const game = setInterval(draw, 15);
 
 pipeBottom.onload = draw;
